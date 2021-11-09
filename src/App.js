aef52794d1524ac4a591a19601e2c82ad6e8c57e
HRITWIK TRIPATHI <Tripathi.hritwik@gmail.com>
2021-11-09 15:21:18 +0530

    more bugs fixed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Top from './components/Top';
+
+function App() {
+  return (
+    <>
+      <Top />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Top.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+
+
+
+
+export default function Top() {
+
+  
+  return (
+      <>
+      <Web3modal />
+      
+    </>
+  );
+}
